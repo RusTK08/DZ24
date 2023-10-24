7820cff119d73afe47bf38c6bbcda2168d5c4797
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-24 22:07:25 +0300

    1Zad

---
+++ DZ/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число!");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2!");
+int num1 = Convert.ToInt32(Console.ReadLine());
+void Proba(int num, int num1)
+{
+    if(num1 > 0)
+    {
+        double pow = Math.Pow(num, num1);
+        int otv = Convert.ToInt32(pow);
+    }
+    else
+    {
+        Console.WriteLine("Введите число 2, которое больше нуля!");
+    }
+}
+int got = Proba();
+Console.WriteLine(got);
+

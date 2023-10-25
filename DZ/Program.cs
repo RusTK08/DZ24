346f0e664996489adb6edfb731d402ae7b0093b7
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-25 19:17:32 +0300

    zad1

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -2,19 +2,20 @@
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2!");
 int num1 = Convert.ToInt32(Console.ReadLine());
-void Proba(int num, int num1)
+int Proba(int num, int num1)
 {
     if(num1 > 0)
     {
         double pow = Math.Pow(num, num1);
         int otv = Convert.ToInt32(pow);
-        Console.WriteLine(otv);
+        return otv;
     }
     else
     {
         Console.WriteLine("Введите число 2, которое больше нуля!");
+        return 0;
     }
 }
-int got = Proba();
+int got = Proba(num, num1);
 Console.WriteLine(got);
 

e745b42ebafff467a44d933b30148b827330aeb8
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-25 21:19:56 +0300

    2zad

---
+++ DZ2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int Digits(int num)
+{
+    if (num == 0)
+        return 1;
+
+    
+
+int result = 0;
+    while(num > 0)
+    {
+        
+        num = num / 10;
+        result++;
+    }
+    
+    return result;
+}
+int digit = Digits(num);
+Console.WriteLine(digit);

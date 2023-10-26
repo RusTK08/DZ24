c291f9c893e81f8794255cc7443497344584c620
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-27 00:25:13 +0300

    3Korrrekt

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,38 +1,38 @@
 ﻿
-// int n = 8;
-// int [] array = new int [n];
-// for(int i = 0; i < n; i++)
-// {
-//     array [i] = new Random().Next(0, 100);
-//     Console.Write(array [i] + " ");
-// }
+int n = 8;
+int [] array = new int [n];
+for(int i = 0; i < n; i++)
+{
+    array [i] = new Random().Next(0, 100);
+    Console.Write(array [i] + " ");
+}
 
 
 // ИЛИ ВОТ ТАК:
 
 
 
-int n = 8;
-int [] GenerateArray(int n)
-{
-    int[] array = new int[n];  //создали  array [] array [] array [] array [] array [] array [] array [] array []
-    for(int i = 0; i < n; i++)
-    {
-    array [i] = new Random().Next(0, 100);
-    }
-    return array;
-}
+// int n = 8;
+// int [] GenerateArray(int n)
+// {
+//     int[] array = new int[n];  //создали  array [] array [] array [] array [] array [] array [] array [] array []
+//     for(int i = 0; i < n; i++)
+//     {
+//     array [i] = new Random().Next(0, 100);
+//     }
+//     return array;
+// }
 
 
-void PrintArray(int [] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " "); // созданным  (array [] array [] array [] array [] array [] array [] array [] array []) добавили пробел и вывели.
-    }
-}
-int [] array = GenerateArray(n);
-PrintArray(array);
+// void PrintArray(int [] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " "); // созданным  (array [] array [] array [] array [] array [] array [] array [] array []) добавили пробел и вывели.
+//     }
+// }
+// int [] array = GenerateArray(n);
+// PrintArray(array);
 
 
 // ИЛИ ВОТ ТАК:
